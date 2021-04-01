db988fee4f68a6d728b3d9b22e92344cf87dbd1e
pomubry <btaduran30@protonmail.com>
2021-04-02 00:47:14 +0800

    added fetchQuery based on queryParams. also added client-side fetching to store additional anime in state

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import fetchQuery from '../lib/fetchQuery';
 import Anime from '../components/Anime';
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps() {
-  const data = await fetchQuery(1);
+  const data = await fetchQuery();
 
   if (!data) {
     return {
@@ -22,21 +23,76 @@ export async function getServerSideProps() {
   };
 }
 
-export default function Home({ media }) {
+export default function Home({ media, pageInfo }) {
   const [animeArr, setAnimeArr] = useState(media);
+  const [pageDetails, setPageDetails] = useState(pageInfo);
+  const [isLoading, setIsLoading] = useState(false);
+  const { query } = useRouter();
+  const { page, season, seasonYear } = query;
+
+  const fetchMore = async () => {
+    if (!pageDetails.hasNextPage) return;
+    setIsLoading(true);
+
+    const data = await fetchQuery(
+      pageDetails.currentPage + 1,
+      season,
+      seasonYear
+    );
+    if (!data) {
+      setIsLoading(false);
+      return;
+    }
+    const { pageInfo, media } = data.data.Page;
+
+    let oldArr = [...animeArr];
+    media.forEach((anime) => {
+      let isUnique = true;
+      oldArr.forEach((obj) => {
+        if (obj.id === anime.id) isUnique = false;
+      });
+      if (isUnique) oldArr.push(anime);
+    });
+
+    setAnimeArr(oldArr);
+    setPageDetails(pageInfo);
+    setIsLoading(false);
+  };
+
+  useEffect(async () => {
+    if (!Object.keys(query).length) return;
+
+    setIsLoading(true);
+    const data = await fetchQuery(page, season, seasonYear);
+    if (!data) {
+      setIsLoading(false);
+      return;
+    }
+
+    const { pageInfo, media } = data.data.Page;
+    setAnimeArr(media);
+    setPageDetails(pageInfo);
+    setIsLoading(false);
+  }, [page, season, seasonYear]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Next.js Ani-Database</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles.title}>Trending Anime</h1>
+      <h1 className={styles.title}>
+        Trending Anime {isLoading && 'Am Loading'}
+      </h1>
 
       <section className={styles.animeContainer}>
-        {animeArr.map((anime) => (
-          <Anime anime={anime} key={anime.id} />
-        ))}
+        {animeArr &&
+          animeArr.map((anime) => <Anime anime={anime} key={anime.id} />)}
       </section>
+      {isLoading && <p>Am Loading</p>}
+      {!isLoading && pageDetails.hasNextPage && (
+        <button onClick={fetchMore}>fetchMore</button>
+      )}
     </div>
   );
 }
