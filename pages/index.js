7a57c18ccce18b59b3f32d7ef7d8a5b2466d5aee
pomubry <btaduran30@protonmail.com>
2021-04-03 02:57:07 +0800

    Added season and year inputs, added MUI for input forms

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { useEffect, useState } from 'react';
-import fetchQuery from '../lib/fetchQuery';
-import Anime from '../components/Anime';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Anime from '../components/Anime';
+import fetchQuery from '../lib/fetchQuery';
+import styles from '../styles/Home.module.css';
+
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core';
 
 export async function getServerSideProps() {
   const data = await fetchQuery();
@@ -23,12 +29,22 @@ export async function getServerSideProps() {
   };
 }
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
 export default function Home({ media, pageInfo }) {
   const [animeArr, setAnimeArr] = useState(media);
   const [pageDetails, setPageDetails] = useState(pageInfo);
   const [isLoading, setIsLoading] = useState(false);
-  const { query } = useRouter();
+  const { query, push } = useRouter();
   const { page, season, seasonYear } = query;
+  const classes = useStyles();
+
+  const keywords = animeArr.map((anime) => anime.title.romaji);
 
   const fetchMore = async () => {
     if (!pageDetails.hasNextPage) return;
@@ -39,22 +55,23 @@ export default function Home({ media, pageInfo }) {
       season,
       seasonYear
     );
+
     if (!data) {
       setIsLoading(false);
       return;
     }
-    const { pageInfo, media } = data.data.Page;
 
-    let oldArr = [...animeArr];
-    media.forEach((anime) => {
-      let isUnique = true;
-      oldArr.forEach((obj) => {
-        if (obj.id === anime.id) isUnique = false;
-      });
-      if (isUnique) oldArr.push(anime);
+    const { pageInfo, media } = data.data.Page;
+    let newArr = [...animeArr];
+
+    media.forEach((animeToAdd) => {
+      let isUnique = newArr.every(
+        (includedAnime) => includedAnime.id !== animeToAdd.id
+      );
+      if (isUnique) newArr.push(animeToAdd);
     });
 
-    setAnimeArr(oldArr);
+    setAnimeArr(newArr);
     setPageDetails(pageInfo);
     setIsLoading(false);
   };
@@ -75,21 +92,82 @@ export default function Home({ media, pageInfo }) {
     setIsLoading(false);
   }, [page, season, seasonYear]);
 
+  const seasonLink = (value) => {
+    let str = '/?';
+    if (seasonYear) str += `seasonYear=${seasonYear}&`;
+    str += `season=${value}`;
+    push(str);
+  };
+
+  const seasonYearLink = (value) => {
+    let str = '/?';
+    if (season) str += `season=${season}&`;
+    str += `seasonYear=${value}`;
+    push(str);
+  };
+
+  const yearList = () => {
+    let arr = [];
+    let start = new Date().getFullYear() + 1;
+    for (let i = start; i >= 1940; i--) {
+      let jsx = (
+        <MenuItem value={i} key={i}>
+          {i}
+        </MenuItem>
+      );
+      arr.push(jsx);
+    }
+    return arr;
+  };
+
   return (
     <div className={styles.container}>
       <Head>
+        <meta name="description" content="Anime list database" />
+        <meta
+          name="keywords"
+          content={`anime list,anime database,${keywords}`}
+        />
+        <meta name="author" content="pomubry" />
         <title>Next.js Ani-Database</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles.title}>
-        Trending Anime {isLoading && 'Am Loading'}
-      </h1>
+
+      <h1 className={styles.title}>Trending Anime</h1>
+
+      <form>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="seasonLabel">Season</InputLabel>
+          <Select
+            labelId="seasonLabel"
+            id="season"
+            value={season || animeArr[0].season}
+            onChange={(e) => seasonLink(e.target.value)}
+          >
+            <MenuItem value="WINTER">Winter</MenuItem>
+            <MenuItem value="SPRING">Spring</MenuItem>
+            <MenuItem value="SUMMER">Summer</MenuItem>
+            <MenuItem value="FALL">Fall</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="seasonYearLabel">Year</InputLabel>
+          <Select
+            labelId="seasonYearLabel"
+            id="seasonYear"
+            value={seasonYear || animeArr[0].seasonYear}
+            onChange={(e) => seasonYearLink(e.target.value)}
+          >
+            {yearList()}
+          </Select>
+        </FormControl>
+      </form>
 
       <section className={styles.animeContainer}>
         {animeArr &&
           animeArr.map((anime) => <Anime anime={anime} key={anime.id} />)}
       </section>
-      {isLoading && <p>Am Loading</p>}
+      {isLoading && <p>Loading. . .</p>}
       {!isLoading && pageDetails.hasNextPage && (
         <button onClick={fetchMore}>fetchMore</button>
       )}
